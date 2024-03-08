1f3a76cb9688b796925a5aff01deff90301b3c09
zenpk <99558412+zenpk@users.noreply.github.com>
2024-03-08 16:41:32 +0000

    update

--- init.lua
+++ init.lua
@@ -799,7 +799,7 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'html', 'typescript', 'python' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'html', 'javascript', 'typescript', 'java', 'json', 'python' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
