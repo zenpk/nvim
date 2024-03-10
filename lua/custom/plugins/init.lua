721bde1b96ae430f37416bfa132b91ac8354139a
zenpk <99558412+zenpk@users.noreply.github.com>
2024-03-10 10:28:15 +0000

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -39,6 +39,49 @@ return {
               --".null-ls_*",
             },
           },
+          commands = {
+            -- https://github.com/nvim-neo-tree/neo-tree.nvim/issues/202
+            -- over write default 'delete' command to 'trash'.
+            delete = function(state)
+              local inputs = require 'neo-tree.ui.inputs'
+              local path = state.tree:get_node().path
+              local msg = 'Are you sure you want to trash ' .. path
+              inputs.confirm(msg, function(confirmed)
+                if not confirmed then
+                  return
+                end
+
+                vim.fn.system { 'trash', vim.fn.fnameescape(path) }
+                require('neo-tree.sources.manager').refresh(state.name)
+              end)
+            end,
+
+            -- over write default 'delete_visual' command to 'trash' x n.
+            delete_visual = function(state, selected_nodes)
+              local inputs = require 'neo-tree.ui.inputs'
+
+              -- get table items count
+              function GetTableLen(tbl)
+                local len = 0
+                for n in pairs(tbl) do
+                  len = len + 1
+                end
+                return len
+              end
+
+              local count = GetTableLen(selected_nodes)
+              local msg = 'Are you sure you want to trash ' .. count .. ' files ?'
+              inputs.confirm(msg, function(confirmed)
+                if not confirmed then
+                  return
+                end
+                for _, node in ipairs(selected_nodes) do
+                  vim.fn.system { 'trash', vim.fn.fnameescape(node.path) }
+                end
+                require('neo-tree.sources.manager').refresh(state.name)
+              end)
+            end,
+          },
         },
       }
     end,
@@ -49,18 +92,30 @@ return {
   },
   {
     -- auto brackets
-    'jiangmiao/auto-pairs',
-  },
-  {
-    'doums/darcula',
-    lazy = false,
-    priority = 1000,
+    'windwp/nvim-autopairs',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
     config = function()
-      vim.cmd.colorscheme 'darcula'
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end,
   },
   {
-    'rktjmp/lush.nvim',
+    -- colorscheme
+    'zenpk/jeteyes',
+    lazy = false,
+    priority = 1000,
+    name = 'jeteyes',
+    config = function()
+      vim.cmd.colorscheme 'jeteyes'
+    end,
   },
   { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
+  {
+    'rktjmp/lush.nvim',
+  },
+  { 'rktjmp/shipwright.nvim' },
 }
