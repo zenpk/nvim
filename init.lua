a06b1052723b612aa860ae1597ac78c23edae690
zenpk <99558412+zenpk@users.noreply.github.com>
2024-03-11 16:41:58 +0800

    overwrite log

--- init.lua
+++ init.lua
@@ -383,13 +383,14 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<C-e>', builtin.find_files, { desc = 'Search Files' })
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Search Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<C-e>', builtin.oldfiles, { desc = 'Search Recent Files' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -605,7 +606,9 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
         'prettier',
-        'eslint'
+        'eslint',
+        'angular-language-server',
+        'vue-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -640,8 +643,10 @@ require('lazy').setup({
         go = { { 'goimport', 'gofmt' } },
         html = { { 'prettierd', 'prettier' } },
         javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
         python = { 'isort', 'black' },
+        typescript = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
       },
     },
   },
@@ -705,6 +710,7 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-Enter>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -807,7 +813,7 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'go', 'html', 'javascript', 'typescript', 'java', 'json', 'python' },
+        ensure_installed = 'all',
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -24,48 +24,81 @@ vim.keymap.set('', '<leader>f', function()
 end)
 
 -- useful for debugging
-function GeneratePrint()
+local function GeneratePrint()
   -- get the current buffer's filetype
   local filetype = vim.bo.filetype
-  if filetype == 'python' or 'lua' then
-    return 'oprint()<left>'
-  elseif filetype == 'javascript' or filetype == 'typescript' then
-    return 'oconsole.log();<left><left>'
+  local printCmd = 'o'
+  if filetype == 'python' or filetype == 'lua' then
+    printCmd = printCmd .. 'print()'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(printCmd .. '<Esc>i', true, false, true), 'n', false)
+  elseif
+    filetype == 'javascript'
+    or filetype == 'typescript'
+    or filetype == 'javascriptreact'
+    or filetype == 'typescriptreact'
+    or filetype == 'vue'
+    or filetype == 'svelte'
+  then
+    printCmd = printCmd .. 'console.log();'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(printCmd .. '<Esc>hi', true, false, true), 'n', false)
   elseif filetype == 'go' then
-    return 'ofmt.Println()<left>'
+    printCmd = printCmd .. 'fmt.Println()'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(printCmd .. '<Esc>i', true, false, true), 'n', false)
   else
-    return 'o'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('o<Esc>', true, false, true), 'n', false)
   end
 end
 
-vim.keymap.set('n', '<leader>gp', GeneratePrint(), { desc = '[G]enerate [P]rint', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gp', GeneratePrint, { desc = '[G]enerate [P]rint', noremap = true, silent = true })
 
-function GenerateLine()
+local function GenerateLine()
   local filetype = vim.bo.filetype
-  local message = '========== DEBUG =========='
-  if filetype == 'python' or 'lua' then
-    return string.format("oprint('%s')<esc>", message)
-  elseif filetype == 'javascript' or filetype == 'typescript' then
-    return string.format('oconsole.log(`%s`);<esc>', message)
+  local message = '==========  =========='
+  local moveCursor = '<Esc>bhi'
+  local printCmd = 'o'
+  if filetype == 'python' or filetype == 'lua' then
+    printCmd = printCmd .. string.format("print('%s')", message)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(printCmd .. moveCursor, true, false, true), 'n', false)
+  elseif
+    filetype == 'javascript'
+    or filetype == 'typescript'
+    or filetype == 'javascriptreact'
+    or filetype == 'typescriptreact'
+    or filetype == 'vue'
+    or filetype == 'svelte'
+  then
+    printCmd = printCmd .. string.format('console.log(`%s`);', message)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(printCmd .. moveCursor, true, false, true), 'n', false)
   elseif filetype == 'go' then
-    return string.format('ofmt.Println("%s")<esc>', message)
+    printCmd = printCmd .. string.format('fmt.Println("%s")', message)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(printCmd .. moveCursor, true, false, true), 'n', false)
   else
-    return 'o'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('o<Esc>', true, false, true), 'n', false)
   end
 end
 
-vim.keymap.set('n', '<leader>gl', GenerateLine(), { desc = '[G]enerate [L]ine', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gl', GenerateLine, { desc = '[G]enerate [L]ine', noremap = true, silent = true })
 
 -- useful for error handling
-function GeneralErr()
+local function GenerateErr()
   local filetype = vim.bo.filetype
-  if filetype == 'javascript' or filetype == 'typescript' then
-    return 'otry {<enter>} catch (e) {<enter>}<esc>kk$o'
+  local errBlock = 'o'
+  if
+    filetype == 'javascript'
+    or filetype == 'typescript'
+    or filetype == 'javascriptreact'
+    or filetype == 'typescriptreact'
+    or filetype == 'vue'
+    or filetype == 'svelte'
+  then
+    errBlock = errBlock .. 'try {<CR><Esc>dda catch (e) {<CR><Esc>kko'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(errBlock, true, true, true), 't', false)
   elseif filetype == 'go' then
-    return 'oif err != nil {<enter>return err<enter>}<enter><esc>'
+    errBlock = errBlock .. 'if err != nil {<CR>return err<Esc>k$o'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(errBlock, true, true, true), 't', false)
   else
-    return 'o'
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('o<Esc>', true, false, true), 'n', false)
   end
 end
 
-vim.keymap.set('n', '<leader>ge', GeneralErr(), { desc = '[G]enerae [E]rror', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ge', GenerateErr, { desc = '[G]enerae [E]rror', noremap = true, silent = true })
